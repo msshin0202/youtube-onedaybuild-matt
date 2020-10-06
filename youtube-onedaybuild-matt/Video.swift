a0c0e3b9ac48d1f650c54e04fdfed325dc1eefbd
Minsoo Shin <msshin0202@gmail.com>
2020-10-05 17:48:44 -0700

    lesson 4 completed

---
+++ youtube-onedaybuild-matt/Video.swift
@@ -0,0 +1,18 @@
+//
+//  Video.swift
+//  youtube-onedaybuild-matt
+//
+//  Created by Matthew Shin on 2020-10-05.
+//
+
+import Foundation
+
+struct Video {
+    
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+    
+}
