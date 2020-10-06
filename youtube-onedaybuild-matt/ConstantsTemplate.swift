75b15d4b2e45735888fdfd3b32f3f81cac9743e5
Minsoo Shin <msshin0202@gmail.com>
2020-10-05 17:50:44 -0700

    added constant template

---
+++ youtube-onedaybuild-matt/ConstantsTemplate.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  youtube-onedaybuild-matt
+//
+//  Created by Matthew Shin on 2020-10-05.
+//
+
+/* Add your own keys below and uncomment the code */
+
+import Foundation
+
+/*
+struct Constants {
+    
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
+    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
+ */
